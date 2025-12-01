71b99f1c10d3abff5002ce3cacaa8df77625ce90
Adolfo Castilho Garcia <adolfo@Adolfo-Imac.local>
2025-11-30 22:08:59 -0300

    Perf: Carregamento assincrono de CSS

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -11,9 +11,20 @@ class MyDocument extends Document {
           {/* meta end */}
 
           {/* public assets begin */}
-          <link rel="stylesheet" href="/css/plugins/bootstrap-grid.css" />
-          <link rel="stylesheet" href="/css/plugins/font-awesome.min.css" />
-          <link rel="stylesheet" href="/css/plugins/swiper.min.css" />
+          <link rel="preload" href="/css/plugins/bootstrap-grid.css" as="style" />
+          <link rel="stylesheet" href="/css/plugins/bootstrap-grid.css" media="print" onLoad="this.media='all'" />
+
+          <link rel="preload" href="/css/plugins/font-awesome.min.css" as="style" />
+          <link rel="stylesheet" href="/css/plugins/font-awesome.min.css" media="print" onLoad="this.media='all'" />
+
+          <link rel="preload" href="/css/plugins/swiper.min.css" as="style" />
+          <link rel="stylesheet" href="/css/plugins/swiper.min.css" media="print" onLoad="this.media='all'" />
+
+          <noscript>
+            <link rel="stylesheet" href="/css/plugins/bootstrap-grid.css" />
+            <link rel="stylesheet" href="/css/plugins/font-awesome.min.css" />
+            <link rel="stylesheet" href="/css/plugins/swiper.min.css" />
+          </noscript>
           {/* public assets end */}
           <link rel="icon" href="/img/icons/icone_bechange.svg" type="image/svg+xml" />
         </Head>
